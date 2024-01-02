5d7ac7f0db1254bf9afd851b64a910416185dfe9
prsousa8 <prsousa.128@gmail.com>
2024-01-02 17:46:40 -0300

    Desafio concluido.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("12344", "RM", "HSHAGH", "56 GB");
+nokia.Ligar();
+nokia.InstalarAplicativo("Zap");
+nokia.ReceberLigacao();
+
+Iphone iphone = new Iphone("15676776", "TYU", "hHJGHGJ", "234 GB");
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
